9523def5d071731e23c64deb60e8169b1ec41725
Matthew Kandiah <mj.kandiah@googlemail.com>
2025-10-05 08:25:06 +0100

    install lsp-mode and set up odin lsp using use-package

--- init.el
+++ init.el
@@ -71,14 +71,19 @@
 ;; - wtf does ivy do?
 ;; - wtf does company actually do?
 
-;; is this the right way to do this?
 (load "~/.config/emacs/odin-mode.el")
-(with-eval-after-load 'lsp-mode
+(use-package lsp-mode
+  :init
+  (setq lsp-keymap-prefix "C-c l")
+  :config
   (setq-default lsp-auto-guess-root t)
-  (setq lsp-language-id-configuration (cons '(odin-mode . "odin") lsp-language-id-configuration))
+  (add-to-list 'lsp-language-id-configuration '(odin-mode . "odin"))
   (lsp-register-client
    (make-lsp-client :new-connection (lsp-stdio-connection "~/.local/bin/ols")
-		    :major-modes '(odin-mode)
+		    :activation-fn (lsp-activate-on "odin")
 		    :server-id 'ols
-		    :multi-root t)))
-(add-hook 'odin-mode-hook #'lsp)
+		    :multi-root t))
+  :hook
+  ((odin-mode . lsp)
+   (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp)
